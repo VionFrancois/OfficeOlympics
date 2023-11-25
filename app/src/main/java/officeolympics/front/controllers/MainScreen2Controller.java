691d3808ab3dc4f97981e08826e3c7f3bf3b8d21
frix <210809@umons.ac.be>
2023-11-25 14:09:18 +0100

    Image clickable (buggé)

--- app/src/main/java/officeolympics/front/controllers/MainScreen2Controller.java
+++ app/src/main/java/officeolympics/front/controllers/MainScreen2Controller.java
@@ -2,13 +2,19 @@ package officeolympics.front.controllers;
 import javafx.fxml.FXML;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 import javafx.fxml.Initializable;
 import javafx.scene.text.Font;
+import officeolympics.Main;
 
 
 public class MainScreen2Controller extends Controller {
+
     @FXML
-    Label titre;
+    void handleNextPage(MouseEvent event){
+        nextScene = scene; // Tkt
+        Main.setScene(nextScene);
+    }
 
 }
