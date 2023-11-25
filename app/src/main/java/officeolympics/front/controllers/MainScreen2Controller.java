9b74c3a7d305f23d96cf14af6d22cb4cf1d89b11
frix <210809@umons.ac.be>
2023-11-25 10:50:47 +0100

    Ajout font (A installer en local !!!)

---
+++ app/src/main/java/officeolympics/front/controllers/MainScreen2Controller.java
@@ -0,0 +1,14 @@
+package officeolympics.front.controllers;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+import javafx.fxml.Initializable;
+import javafx.scene.text.Font;
+
+
+public class MainScreen2Controller extends Controller {
+    @FXML
+    Label titre;
+
+}
